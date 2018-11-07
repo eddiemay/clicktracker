f152c675aae4c9b0f838ee2b93754cf42bbe7852
eddiemay <eddiemay1999@yahoo.com>
2018-11-06 20:57:41 -0800

    Update for server/service refactor.

--- src/main/java/com/digitald4/clicktracker/server/ApiServiceServlet.java
+++ src/main/java/com/digitald4/clicktracker/server/ApiServiceServlet.java
@@ -1,8 +1,8 @@
 package com.digitald4.clicktracker.server;
 
 import com.digitald4.clicktracker.proto.ClickTrackerProtos.Click;
-import com.digitald4.common.server.JSONServiceImpl;
-import com.digitald4.common.server.SingleProtoService;
+import com.digitald4.common.server.service.JSONServiceImpl;
+import com.digitald4.common.server.service.SingleProtoService;
 import com.digitald4.common.storage.GenericStore;
 
 public class ApiServiceServlet extends com.digitald4.common.server.ApiServiceServlet {
